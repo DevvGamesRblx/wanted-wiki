f5663c061bd6192645a10eff2412345881dd9508
DevvGames <102315175+DevvGamesRblx@users.noreply.github.com>
2026-02-02 18:09:13 +0200

    Mobile QoL changes and Search Bar

---
+++ data/contributors.js
@@ -0,0 +1,14 @@
+const CONTRIBUTORS_DATA = {
+    managers: [
+        { name: "DavyDevv", title: "Wiki Manager" }
+    ],
+    staff: [
+        { name: "", title: "" },
+        { name: "", title: "" }
+    ],
+    contributors: [
+        { name: "", contributions: "" },
+        { name: "", contributions: " " },
+        { name: "", contributions: "" }
+    ]
+};
